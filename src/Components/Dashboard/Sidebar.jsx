7e71a2ee7c5bb42f249422ac18dffef2b38c9edb
tawhidislam22 <tawhidislam144@gmail.com>
2025-05-15 23:21:33 +0600

    added:dashboard section back button added

--- src/Components/Dashboard/Sidebar.jsx
+++ src/Components/Dashboard/Sidebar.jsx
@@ -64,6 +64,7 @@ const Sidebar = () => {
                   width='100'
                   height='100'
                 /> */}
+                Back
               </Link>
             </div>
           </div>
